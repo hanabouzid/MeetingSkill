dd5dc764e0070e10b961139062553890fb8cb49f
hana bouzid <hana.bouzid95@gmail.com>
2020-06-01 17:15:08 +0100

    test5

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 from mycroft.util.parse import extract_datetime
+from mycroft.util import extract_number
 from datetime import datetime, timedelta
 import pickle
 import os.path
@@ -13,9 +14,11 @@ from googleapiclient.discovery import build
 from oauth2client.file import Storage
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client import tools
+
 import string
 import pytz
 #in the raspberry we add __main__.py for the authorization
+
 UTC_TZ = u'+00:00'
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 FLOW = OAuth2WebServerFlow(
@@ -56,21 +59,6 @@ class CreateEvent(MycroftSkill):
                 pickle.dump(creds, token)
 
         service = build('calendar', 'v3', credentials=creds)
-
-        # Call the Calendar API
-        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
-
-        if not events:
-            print('No upcoming events found.')
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
-
         storage = Storage('info.dat')
         credentials = storage.get()
         if credentials is None or credentials.invalid == True:
@@ -103,24 +91,83 @@ class CreateEvent(MycroftSkill):
         datend = et.strftime('%Y-%m-%dT%H:%M:00')
         datend += UTC_TZ
         datestart += UTC_TZ
-
-        #adding attendees
         # getting contacts emails and names in two lists nameliste and adsmails
         nameListe = []
         adsmails = []
-        #attendee est la liste des invités qui sont disponibles
-        attendee=[]
+        # attendee est la liste des invités qui sont disponibles
+        attendee = []
 
         for person in connections:
             emails = person.get('emailAddresses', [])
             names = person.get('names', [])
             adsmails.append(emails[0].get('value'))
             nameListe.append(names[0].get('displayName'))
+
+        # liste des emails de toutes les salles de focus
+        freemails = []
+        freerooms = []
+        nameroom = ["Midoune Meeting Room", "Aiguilles Meeting Room", "Barrouta Meeting Room", "Kantaoui Meeting Room",
+                    "Gorges Meeting Room", "Ichkeul Meeting Room", "Khemir Meeting Room", "Tamaghza Meeting Room",
+                    "Friguia Meeting Room", "Ksour Meeting Room", "Medeina Meeting Room", "Thyna Meeting Room"]
+        emailroom = ["focus-corporation.com_3436373433373035363932@resource.calendar.google.com",
+                     "focus-corporation.com_3132323634363237333835@resource.calendar.google.com",
+                     "focus-corporation.com_3335353934333838383834@resource.calendar.google.com",
+                     "focus-corporation.com_3335343331353831343533@resource.calendar.google.com",
+                     "focus-corporation.com_3436383331343336343130@resource.calendar.google.com",
+                     "focus-corporation.com_36323631393136363531@resource.calendar.google.com",
+                     "focus-corporation.com_3935343631343936373336@resource.calendar.google.com",
+                     "focus-corporation.com_3739333735323735393039@resource.calendar.google.com",
+                     "focus-corporation.com_3132343934363632383933@resource.calendar.google.com",
+                     "focus-corporation.com_@resource.calendar.google.com",
+                     "focus-corporation.com_@resource.calendar.google.com",
+                     "focus-corporation.com_@resource.calendar.google.com"]
+        for i in range(0, len(emailroom)):
+            body = {
+                "timeMin": datestart,
+                "timeMax": datend,
+                "timeZone": 'America/Los_Angeles',
+                "items": [{"id": emailroom[i]}]
+            }
+            roomResult = service.freebusy().query(body=body).execute()
+            room_dict = roomResult[u'calendars']
+            for cal_room in room_dict:
+                print(cal_room, ':', room_dict[cal_room])
+                case = room_dict[cal_room]
+                for j in case:
+                    if (j == 'busy' and case[j] == []):
+                        # la liste freerooms va prendre  les noms des salles free
+                        freerooms.append(nameroom[i])
+                        freemails.append(emailroom[i])
+        suggroom = freerooms[0]
+        suggmail = freemails[0]
+        reservation = self.get_response('do you need to make a reservation for a meeting room? Yes or No?')
+        if reservation == 'yes':
+
+            self.speak_dialog("suggestionroom", data={"suggroom": suggroom})
+            x = self.get_response("Do you agree making a reservation for this meeting room")
+            if x == "yes":
+                room = suggroom
+                attendee.append(suggmail)
+            else:
+                s = ",".join(freerooms)
+                # print("les salles disponibles a cette date sont", freerooms)
+                self.speak_dialog("freerooms", data={"s": s})
+                room = self.get_response('which Room do you want to make a reservation for??')
+                for i in range(0, len(freerooms)):
+                    if (freerooms[i] == room):
+                        # ajouter l'email de room dans la liste des attendees
+                        attendee.append(freemails[i])
+        else:
+            room = ''
+
+        #adding attendees
+
         #verify if the attendee in the connection liste and if he is free
         confirm= self.get_response("Do you want to invite someone? yes or no?")
         if confirm =='yes':
             n_attendee = self.get_response(" how many persons would you like to invite")
-            n = int(n_attendee )
+            n = extract_number(n_attendee)
+            nb=n
             print(n)
             j = 0
             while j < n:
@@ -148,12 +195,12 @@ class CreateEvent(MycroftSkill):
                             statut = cal_dict[cal_name]
                             for i in statut:
                                 if (i == 'busy' and statut[i] == []):
-                                    self.speak_dialog("free")
+                                    self.speak_dialog("free",data={"att":x})
                                     attendee.append(mail)
                                     #ajouter l'email de x ala liste des attendee
                                 elif (i == 'busy' and statut[i] != []):
-                                    self.speak_dialog("busy")
-
+                                    self.speak_dialog("busy",data={"att":x})
+                                    nb -= 1
                 if exist == False:
                     self.speak_dialog("notexist")
                 j += 1
@@ -167,41 +214,7 @@ class CreateEvent(MycroftSkill):
             notif = True,
         else:
             notif = False
-        #liste des emails de toutes les salles de focus
-        freemails=[]
-        freerooms = []
-        nameroom =["Midoune Meeting Room","Aiguilles Meeting Room","Barrouta Meeting Room","Kantaoui Meeting Room","Gorges Meeting Room","Ichkeul Meeting Room","Khemir Meeting Room","Tamaghza Meeting Room","Friguia Meeting Room","Ksour Meeting Room","Medeina Meeting Room","Thyna Meeting Room"]
-        emailroom=["focus-corporation.com_3436373433373035363932@resource.calendar.google.com","focus-corporation.com_3132323634363237333835@resource.calendar.google.com","focus-corporation.com_3335353934333838383834@resource.calendar.google.com","focus-corporation.com_3335343331353831343533@resource.calendar.google.com","focus-corporation.com_3436383331343336343130@resource.calendar.google.com","focus-corporation.com_36323631393136363531@resource.calendar.google.com","focus-corporation.com_3935343631343936373336@resource.calendar.google.com","focus-corporation.com_3739333735323735393039@resource.calendar.google.com","focus-corporation.com_3132343934363632383933@resource.calendar.google.com","focus-corporation.com_@resource.calendar.google.com", "focus-corporation.com_@resource.calendar.google.com","focus-corporation.com_@resource.calendar.google.com"]
-        for i in range(0, len(emailroom)):
-            body = {
-                "timeMin": datestart,
-                "timeMax": datend,
-                "timeZone": 'America/Los_Angeles',
-                "items": [{"id": emailroom[i]}]
-            }
-            roomResult = service.freebusy().query(body=body).execute()
-            room_dict = roomResult[u'calendars']
-            for cal_room in room_dict:
-                print(cal_room, ':', room_dict[cal_room])
-                case = room_dict[cal_room]
-                for j in case:
-                    if (j == 'busy' and case[j] == []):
-                        # la liste freerooms va prendre  les noms des salles free
-                        freerooms.append(nameroom[i])
-                        freemails.append(emailroom[i])
 
-        reservation = self.get_response('do you need to make a reservation for a meeting room? Yes or No?')
-        if reservation == 'yes':
-            s = ",".join(freerooms)
-            #print("les salles disponibles a cette date sont", freerooms)
-            self.speak_dialog("freerooms",data={"s":s})
-            room = self.get_response('which Room do you want to make a reservation for??')
-            for i in range(0, len(freerooms)):
-                if (freerooms[i] == room):
-                    #ajouter l'email de room dans la liste des attendees
-                    attendeess.append({'email': freemails[i]})
-        else:
-            room =''
         #creation d'un evenement
         event = {
             'summary': tittle,
@@ -227,8 +240,20 @@ class CreateEvent(MycroftSkill):
                 ],
             },
         }
-        event = service.events().insert(calendarId='primary',sendNotifications=notif, body=event).execute()
-        print('Event created: %s' % (event.get('htmlLink')))
-        self.speak_dialog("eventCreated")
+        if nb == 0:
+            self.speak_dialog("cancellEvent")
+        elif nb == n:
+            event = service.events().insert(calendarId='primary', sendNotifications=True, body=event).execute()
+            print('Event created: %s' % (event.get('htmlLink')))
+            self.speak_dialog("eventCreated")
+        else:
+            res = self.get_response('Some of the attendees are busy would you like to continue creating the event yes or no?')
+            if res == 'yes':
+                event = service.events().insert(calendarId='primary', sendNotifications=True, body=event).execute()
+                print('Event created: %s' % (event.get('htmlLink')))
+                self.speak_dialog("eventCreated")
+            elif res == 'no':
+                self.speak_dialog("eventCancelled")
+
 def create_skill():
     return CreateEvent()
