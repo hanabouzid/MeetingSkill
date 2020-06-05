b50c76a6e72b1b7422204d30786a021a52d59470
hana bouzid <hana.bouzid95@gmail.com>
2020-06-05 19:20:30 +0100

    test8

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 FLOW = OAuth2WebServerFlow(
     client_id='73558912455-smu6u0uha6c2t56n2sigrp76imm2p35j.apps.googleusercontent.com',
     client_secret='0X_IKOiJbLIU_E5gN3NefNns',
-    scope='https://www.googleapis.com/auth/contacts.readonly',
+    scope=['https://www.googleapis.com/auth/calendar','https://www.googleapis.com/auth/contacts.readonly'],
     user_agent='Smart assistant box')
 
 class CreateEvent(MycroftSkill):
@@ -39,38 +39,18 @@ class CreateEvent(MycroftSkill):
 
     @intent_handler(IntentBuilder("").require("create_event"))
     def createEventt(self):
-        creds = None
-        # The file token.pickle stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    '/opt/mycroft/skills/meetingskill.hanabouzid/client_secret.json', SCOPES)       #dans mycroft on met '/opt/mycroft/skills/createeventskill.hanabouzid/credentials.json'
-                creds = flow.run_local_server(port=0)
-                # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
-
-        service = build('calendar', 'v3', credentials=creds)
-        storage = Storage('info.dat')
-        credentials = storage.get()
+        storage1 = Storage('info3.dat')
+        credentials = storage1.get()
         if credentials is None or credentials.invalid == True:
-            credentials = tools.run_flow(FLOW, storage)
+            credentials = tools.run_flow(FLOW, storage1)
+        print(credentials)
         # Create an httplib2.Http object to handle our HTTP requests and
         # authorize it with our good Credentials.
         http = httplib2.Http()
         http = credentials.authorize(http)
-        # Build a service object for interacting with the API. To get an API key for
-        # your application, visit the Google API Console
-        # and look at your application's credentials page.
+        service = build('calendar', 'v3', http=http)
         people_service = build(serviceName='people', version='v1', http=http)
+        print("authorized")
         # To get a list of people in the user's contacts,
         results = people_service.people().connections().list(resourceName='people/me', pageSize=100,
                                                              personFields='names,emailAddresses',
